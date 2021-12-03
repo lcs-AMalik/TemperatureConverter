1b061969886cda1f33e5a21307ffafecfbd00863
Abdul <AMalik@lcs.on.ca>
2021-12-03 08:40:39 -0500

    Initial Commit

---
+++ TemperatureConverter/TemperatureConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterApp.swift
+//  TemperatureConverter
+//
+//  Created by Abdul Malik on 2021-12-03.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
